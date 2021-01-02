218207dcd360d7d1db8840987cb32a35156cb33a
ainhoaosa <72816088+ainhoaosa@users.noreply.github.com>
2021-01-02 18:23:23 +0100

    Update node.py
    añadido sensor de co2

--- cerradura/node.py
+++ cerradura/node.py
@@ -2,13 +2,28 @@ from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
 from grove.factory import Factory
 import time
 from datetime import datetime
+import math
+import sys
+import time
+from grove.adc import ADC
+ 
 sonar = GroveUltrasonicRanger(5)
 boton_pin = 18
 tiempo=0
 button = Factory.getButton("GPIO-HIGH", boton_pin)
 contador=0
+class GroveGasSensorMQ2:
+ 
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+ 
+    @property
+    def MQ2(self):
+        value = self.adc.read(self.channel)
+        return value
 print('Detecting distance...')
-
+sensor = GroveGasSensorMQ2(0)
 while True:
     
     distancia = sonar.get_distance()
@@ -30,8 +45,9 @@ while True:
             distancia = sonar.get_distance()
             time.sleep(1)
             pass
-        if distancia<=50:
-            contador= contador+1
+        if distancia<=50 and contador>0:
+            contador= contador-1
     tiempo=0
+    print('Gas value: {0}'.format(sensor.MQ2))
     print(contador)
     time.sleep(1)
