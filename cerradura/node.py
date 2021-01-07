faadad197fc0f8d22570dc52144b9833eb82777a
ainhoaosa <72816088+ainhoaosa@users.noreply.github.com>
2021-01-07 18:05:28 +0100

    Add files via upload

--- cerradura/node.py
+++ cerradura/node.py
@@ -10,10 +10,29 @@ import pytz
 from influxdb import InfluxDBClient
 sonar = GroveUltrasonicRanger(5)
 boton_pin = 18
-tiempo=0
+
 button = Factory.getButton("GPIO-HIGH", boton_pin)
-contador=0
+
+global maxi
+maxi = 30
+global porcentaje
+porcentaje = 60
 client = InfluxDBClient('localhost', 8086, 'root', 'root', 'cerradura')
+def aforo():
+    global porcentaje
+    global maxi
+    print("El aforo maximo de la tienda es" ,maxi, " y la restricción es del", porcentaje, "%")
+    print("Es correcto, S/N")
+    respuesta = input()
+    if respuesta =='S':
+        pass
+    else:
+        print("Seleccione el aforo maximo")
+        maxi = int(input())
+        print("Seleccione la restrcción")
+        porcentaje = int(input())
+    
+    
 class GroveGasSensorMQ2:
  
     def __init__(self, channel):
@@ -24,49 +43,79 @@ class GroveGasSensorMQ2:
     def MQ2(self):
         value = self.adc.read(self.channel)
         return value
-print('Detecting distance...')
-sensor = GroveGasSensorMQ2(0)
-while True:
-    
-    distancia = sonar.get_distance()
-    d1 = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    
-    print("date and time =", d1)
-    print(distancia)
-    if distancia<=50:
-        while tiempo<30 and button.is_pressed()==0:
-            tiempo= tiempo+1
-            time.sleep(1)
-            pass
-        if button.is_pressed():
-            contador= contador+1
-    elif button.is_pressed():
-        print("saliendo")
-        while tiempo<30 and distancia>=50:
-            tiempo= tiempo+1
-            distancia = sonar.get_distance()
-            time.sleep(1)
-            pass
-        if distancia<=50 and contador>0:
-            contador= contador-1
+
+def main():
+    sensor = GroveGasSensorMQ2(0)
     tiempo=0
-    print('Gas value: {0}'.format(sensor.MQ2))
-    gas= sensor.MQ2
-    print(contador)
-    json_data = [{
-        "measurement": "contador",
-        "time": d1,
-        "fields": {
-            "value": contador
-        }
-    }]
-    client.write_points(json_data)
-    json_data = [{
-        "measurement": "co2",
-        "time": d1,
-        "fields": {
-            "value": gas
-        }
-    }]
-    client.write_points(json_data)
-    time.sleep(1)
+    contador=0
+    calidad=1
+    
+    global maxi
+    maxi = 30
+    global porcentaje
+    porcentaje = 60
+    aforo()
+    maxi= int(maxi*(porcentaje/100))
+    print(maxi)
+    while True:
+        
+        distancia = sonar.get_distance()
+        d1 = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        
+        print("date and time =", d1)
+        print(distancia)
+        if distancia<=50:
+            while tiempo<10 and button.is_pressed()==0:
+                tiempo= tiempo+1
+                time.sleep(1)
+                pass
+            if button.is_pressed() and contador <maxi:
+                contador= contador+1
+        elif button.is_pressed():
+            print("saliendo")
+            while tiempo<10 and distancia>=50:
+                tiempo= tiempo+1
+                distancia = sonar.get_distance()
+                time.sleep(1)
+                pass
+            if distancia<=50 and contador>0:
+                contador= contador-1
+        tiempo=0
+        print('Gas value: {0}'.format(sensor.MQ2))
+        gas= sensor.MQ2
+        print(contador)
+        json_data = [{
+            "measurement": "contador",
+            "time": d1,
+            "fields": {
+                "value": contador
+            }
+        }]
+        if gas<500:
+            calidad =1
+        elif gas<1000:
+            calidad = 2
+        else:
+            calidad = 3
+        
+        client.write_points(json_data)
+        json_data = [{
+            "measurement": "co2",
+            "time": d1,
+            "fields": {
+                "value": calidad
+            }
+        }]
+        client.write_points(json_data)
+        
+        json_data = [{
+            "measurement": "max",
+            "time": d1,
+            "fields": {
+                "value": maxi
+            }
+            }]
+        client.write_points(json_data)
+        time.sleep(0.5)
+if __name__ == '__main__':
+    main()
